e923d0acf68c08a4d7555ac610f12a3678e72ea6
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-10-11 23:49:53 +0000

    Update logout.php

--- datasphere/controllers/logout.php
+++ datasphere/controllers/logout.php
@@ -1,4 +1,4 @@
-                      <?php
+                     <?php
 require_once '../includes/session.php';
 
 // Destroy all session data
@@ -20,5 +20,6 @@ $_SESSION['temp_logout'] = true;
 header("Location: ../views/login.php");
 exit();
 ?>
+
 
 
