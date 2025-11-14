7bce134e1f88c4186aba38641ad3857fd8a7b9bf
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-11-14 00:14:29 +0000

    Update FeedbackManager.php

--- UnitTest_Files/services/FeedbackManager.php
+++ UnitTest_Files/services/FeedbackManager.php
@@ -1,4 +1,4 @@
-                                    
+                                     
 <?php
 class FeedbackManager {
     private $conn;
@@ -137,6 +137,7 @@ class FeedbackManager {
         
 }
 ?>
+
 
 
 
