824ab43e6cb6e6081b6e1a2ead0a1a2aad0d86da
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-12-06 23:58:49 +0000

    Update footer.php

--- datasphere/includes/footer.php
+++ datasphere/includes/footer.php
@@ -1,8 +1,9 @@
-</main>
+ </main>
     <footer>
         <p>&copy; <?php echo date('Y'); ?> DataSphere. All rights reserved.</p>
     </footer>
     <script src="../assets/js/scripts.js"></script>
     <script src="../assets/js/smooth-scroll.js"></script>
 </body>
+
 </html>
